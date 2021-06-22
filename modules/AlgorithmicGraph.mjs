64ac254d566b22ac5bc7e992eef8af39eb970116
met4citizen <mika.t.suominen@gmail.com>
2021-06-22 20:54:31 +0300

    Fibonacci distance and sphere algorithms revised

--- modules/AlgorithmicGraph.mjs
+++ modules/AlgorithmicGraph.mjs
@@ -37,7 +37,7 @@ class AlgorithmicGraph extends Hypergraph  {
   * @param {number} b Coordinates for point B
   * @return {number} Distance.
   */
-  static dist(a,b) {
+  static d(a,b) {
     let sum = 0;
     for (let i = a.length - 1; i >= 0; i--) {
       sum += Math.pow(a[i] - b[i], 2);
@@ -62,23 +62,22 @@ class AlgorithmicGraph extends Hypergraph  {
   }
 
   /**
-  * Return a spherical Fibonacci lattice with 'samples' points.
-  * @param {number} samples Number of points on the sphere.
-  * @param {number} radius Radius of the sphere.
-  * @return {number[][]} Coordinates of the points.
+  * Return the distance of two points on a Fibonacci sphere radius = 1
+  * @param {number} i Index of point 1
+  * @param {number} j Index of point 2
+  * @param {number} n Total number of points 1
+  * @param {number} m Total number of points 2
+  * @return {number} Distance on a unit sphere
   */
-  static fibonacciSphere(samples, radius = 1 ) {
-    let points = [];
-    let phi = Math.PI * (3.0 - Math.sqrt(5.0))  // golden angle in radians
-    for (var i = 0; i < samples; i++) {
-      let y = 1 - (i / (samples - 1)) * 2;  // y goes from 1 to -1
-      let distance = Math.sqrt( 1 - y * y );  // distance at y
-      let theta = phi * i;  // golden angle increment
-      let x = Math.cos(theta) * distance;
-      let z = Math.sin(theta) * distance;
-      points.push( [ radius * x, radius * y, radius * z ] );
+  static fibonacciD(i, j, n, m) {
+    // Point on a spherical Fibonacci lattice
+    function p(x,n) {
+      let phi = Math.PI * (3.0 - Math.sqrt(5.0))  // golden angle in radians
+      let y = 1 - ( x / (n-1)) * 2;
+      let d = Math.sqrt( 1 - y*y );
+      return [ Math.cos(phi*x)*d, y ,Math.sin(phi*x)*d ];
     }
-    return points;
+    return AlgorithmicGraph.d( p(i,n), p(j,m) );
   }
 
   /**
@@ -92,7 +91,7 @@ class AlgorithmicGraph extends Hypergraph  {
     let edges = [];
     for ( let i = manifold.length-1; i>0; i--) {
       for( let j = i-1; j>=0; j-- ) {
-        if ( Math.abs( AlgorithmicGraph.dist( manifold[i], manifold[j] )) < distance ) {
+        if ( Math.abs( AlgorithmicGraph.d( manifold[i], manifold[j] )) < distance ) {
           // Random direction of the edge
           Math.random() > 0.5 ? edges.push( [i,j] ) : edges.push( [j,i] );
         }
@@ -108,7 +107,7 @@ class AlgorithmicGraph extends Hypergraph  {
   */
   points( n ) {
     if ( (n < 10) || (n > 10000) )
-      throw new Error("Number of points must be between 10-10000.");
+      throw new Error("[Points] Number of points must be between 10-10000.");
 
     let edges = [];
     for( let i=0; i<n; i++) {
@@ -127,9 +126,9 @@ class AlgorithmicGraph extends Hypergraph  {
   */
   grid( n, dimension ) {
     if ( (n < 1) || (n > 10000) )
-      throw new Error("Number of points must be between 1-10000.");
+      throw new Error("[Grid] Number of points must be between 1-10000.");
     if ( (dimension < 1) || (dimension > 10) )
-      throw new Error("Dimension must be between 1-10.");
+      throw new Error("[Grid] Dimension must be between 1-10.");
 
     // Calculate one edge based on dimension
     let size = Math.ceil( Math.pow(n, 1 / dimension ) );
@@ -142,23 +141,6 @@ class AlgorithmicGraph extends Hypergraph  {
   }
 
   /**
-  * Produces a random sprinkling of points into a flat sphere of
-  * given number of vertices.
-  * @param {number} n Number of vertices
-  * @return {number[][]} Edges
-  */
-  sphere( n ) {
-    if ( (n < 10) || (n > 10000) )
-      throw new Error("Number of points must be between 10-10000.");
-
-    // Sprinkling
-    let p = AlgorithmicGraph.fibonacciSphere( n );
-    let dist = AlgorithmicGraph.dist( p[1], p[2] );
-
-    return this.manifoldToGraph( p, 1.1 * dist );
-  }
-
-  /**
   * Produces a random graph of n vertices using sprinkling
   * @param {number} n Number of vertices
   * @param {number} dimension Mininum number of edges per vertix
@@ -167,13 +149,13 @@ class AlgorithmicGraph extends Hypergraph  {
   * @param {number} exp If TRUE use exponential distribution
   * @return {number[][]} Edges
   */
-  random( n, dimension, connections, mode = 0, exp = false ) {
+  random( n, dimension, connections ) {
     if ( (n < 10) || (n > 1000) )
-      throw new Error("Number of points must be between 10-1000.");
+      throw new Error("[Random] Number of points must be between 10-1000.");
     if ( (dimension < 1) || (dimension > 20) )
-      throw new Error("Dimension must be between 1-20.");
+      throw new Error("[Random] Dimension must be between 1-20.");
     if ( (connections < 1) || (connections > 100) )
-      throw new Error("Connections must be between 1-100.");
+      throw new Error("[Random] Connections must be between 1-100.");
 
     // Sprinkling
     let points = [];
@@ -181,18 +163,12 @@ class AlgorithmicGraph extends Hypergraph  {
     let origo = Array( dimension ).fill(0);
     for ( let i = 0; i < n; i++ ) {
       // Random point; if mode==1 filter out points outside the n-ball
-      let point;
-      do {
-        point = Array( dimension ).fill().map(() => {
-          let v = 2 * Math.random() - 1;
-          return exp ? v*v : v;
-        });
-      } while ( (mode == 1) && ( AlgorithmicGraph.dist( point, origo ) > 1 ) );
+      let point = Array( dimension ).fill().map(() => 2 * Math.random() - 1 );
       points.push( point );
 
       // All possible edges using random direction
       for ( let j = 0; j < i; j++ ) {
-        let dist = AlgorithmicGraph.dist( points[i], points[j] );
+        let dist = AlgorithmicGraph.d( points[i], points[j] );
         let edge = Math.random() > 0.5 ? [i,j] : [j,i]; // Random direction
         alledges.push( { edge: edge, dist: dist } );
       }
@@ -217,19 +193,76 @@ class AlgorithmicGraph extends Hypergraph  {
   }
 
   /**
-  * Produces a complete graph with n vertices
+  * Produces a black hole (EXPERIMENTAL)
   * @param {number} n Number of vertices
+  * @param {number} dim Dimension
+  * @param {number} curv Curvature
   * @return {number[][]} Edges
   */
-  complete( n ) {
-    if ( (n < 10) || (n > 100) )
-      throw new Error("Number of vertices must be between 10-100.");
+  blackhole() {
+    let edges = [ [0] ];
+    let numlayers = 4;
+    let vertices = [ 10, 32, 32, 64 ];
+    let separation = [ 1, 2, 2, 3 ];
+    let surface = [ false, true, true, true ]
+    let connections = [ 16, 1, 3, 1 ];
+
+    // Create layers
+    for( let i = 0; i < numlayers; i++ ) {
+      edges.push( ...this.complete( vertices[i], separation[i], surface[i] ).map( e => e.map( x => x + 1000 * i ) ) );
+    }
+
+    // Connect layers
+    for( let i = 0; i < numlayers-1; i++ ) {
+      for( let j = 0; j < vertices[i]; j++ ) {
+        let d = [];
+        for( let k = 0; k < vertices[i+1]; k++ ) {
+          d.push( AlgorithmicGraph.fibonacciD( j, k, vertices[i], vertices[i+1] ) );
+        }
+        let idx = Array.from( Array(d.length).keys() ).sort((a,b) => d[a] - d[b] );
+        for( let k = 0; k < connections[i]; k++ ) {
+          let [ a,b ] = Math.random() > 0.5 ? [j + 1000 * i, idx[k] + 1000 * (i+1)] : [idx[k] + 1000 * (i+1),j + 1000 * i];
+          edges.push( [a,b] );
+        }
+      }
+    }
+
+    return edges;
+  }
+
+  /**
+  * Produces a complete graph with n vertices
+  * @param {number} n Number of vertices
+  * @param {number} d The number of egdes between vertices
+  * @param {number} surface If true, connect only the surface
+  * @return {number[][]} Edges
+  */
+  complete( n, d = 1, surface = false ) {
+    let fibDistRef = 0.0; // Reference distance on a Fibonacci sphere
+    if ( surface ) {
+      fibDistRef = 1.1 * AlgorithmicGraph.fibonacciD(1,2,n,n);
+      if ( (n < 5) || (n > 1000) )
+        throw new Error("[Sphere] Number of vertices must be between 5-1000.");
+    } else {
+      if ( (n < 1) || (n > 100) )
+        throw new Error("[Complete] Number of vertices must be between 1-100.");
+    }
 
     let edges = [];
+    let v = n;
     for( let i=0; i<n-1; i++ ) {
       for( let j=i+1; j<n; j++ ) {
+        // Continue if the points are not near each other on a surface of a sphere
+        if ( surface && AlgorithmicGraph.fibonacciD(i,j,n,n) > fibDistRef ) continue;
+
         // Random direction of the edge
-        Math.random() > 0.5 ? edges.push( [i,j] ) : edges.push( [j,i] );
+        let [ a,b ] = Math.random() > 0.5 ? [i,j] : [j,i];
+
+        //
+        let c = [ a, ...Array(d-1).fill().map( x => v++ ), b ];
+        for( let k = 0; k < c.length-1; k++ ) {
+          edges.push( [ c[k], c[k+1 ] ] );
+        }
       }
     }
 
@@ -257,7 +290,7 @@ class AlgorithmicGraph extends Hypergraph  {
     .replace( /(;)+/g , ";" ).replace( /;$/g ,"" );
 
     // Discard all unsupported characters
-    str = str.replace( /[^()a-z0-9,=;>\.]+/g , "" );
+    str = str.replace( /[^()a-z0-9,=;>\.\-]+/g , "" );
 
     // Expand equal signs == as two separate reversible rules
     let rulestr = "";
@@ -277,7 +310,8 @@ class AlgorithmicGraph extends Hypergraph  {
     });
 
     // To json format, '>' is the separator between lhr/rhs
-    rulestr = rulestr.replace( /;$/g ,"" ).replace( /\),\(/g , ")(" )
+    rulestr = rulestr.replace( /\-/g, "")
+    .replace( /;$/g ,"" ).replace( /\),\(/g , ")(" )
     .replace( /^\(/g , "[{\"lhs\": [[\"" ).replace( /,/g , "\",\"" )
     .replace( /\);\(/g , "\"]]},{\"lhs\": [[\"" )
     .replace( /\)>\(/g , "\"]],\"rhs\": [[\"" )
@@ -346,13 +380,13 @@ class AlgorithmicGraph extends Hypergraph  {
           this.initial.push( ...this.grid( parseInt(params[0]), parseInt(params[1]) ) );
           break;
         case "sphere":
-          this.initial.push( ...this.sphere( parseInt(params[0]) ) );
+          this.initial.push( ...this.complete( parseInt(params[0]), 1, true ) );
           break;
-        case "ball": case "nball":
-          this.initial.push( ...this.random( parseInt(params[0]), parseInt(params[1]), parseInt(params[2]), 1, params.includes("exp") ) );
+        case "blackhole":
+          this.initial.push( ...this.blackhole() );
           break;
         case "random":
-          this.initial.push( ...this.random( parseInt(params[0]), parseInt(params[1]), parseInt(params[2]), 0, params.includes("exp") ) );
+          this.initial.push( ...this.random( parseInt(params[0]), parseInt(params[1]), parseInt(params[2]) ) );
           break;
         case "complete":
           this.initial.push( ...this.complete( parseInt(params[0]) ) );
@@ -366,16 +400,15 @@ class AlgorithmicGraph extends Hypergraph  {
     });
 
     // use first lhs as the initial state, if not specified
+    // Note: replace all vertices with pattern 1, e.g. (1,2) => (1,1)
     if ( this.rules.length && this.initial.length === 0 ) {
-      this.initial.push( ...this.rules[0].lhs );
+      this.initial.push( ...this.rules[0].lhs.map( e => e.map( x => 1 )) );
     }
 
     // Normalize
     this.initial = this.initial.map( e => e.map(String) );
     unique = [ ...new Set( [ ...this.initial.flat() ] ) ];
-    this.initial.forEach( (w,j) => {
-      for( k=0; k < w.length; k++ ) this.initial[j][k] = unique.indexOf( w[k] );
-    });
+    this.initial = this.initial.map( e => e.map( x => unique.indexOf( x ) ) );
     this.initial.sort( (a,b) => Math.min( ...a ) - Math.min( ...b ) );
 
     // Check if there was a valid rule
